038b1d935105100e915c950de3b694ea80f0bfab
Renan2805 <renansilva.dev@gmail.com>
2023-11-27 18:00:41 -0300

    hotfix

--- src/main/java/com/adera/enums/CommandEnum.java
+++ src/main/java/com/adera/enums/CommandEnum.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import java.util.ArrayList;
 
 public enum CommandEnum {
-    RESTART(1, "restart", new String[]{"shutdown /r /t 10"}, new String[]{"sudo systemctl reboot && sleep 10"}),
+    RESTART(1, "restart", new String[]{"shutdown /r /t 10"}, new String[]{"systemctl reboot"}),
     OPTIMIZE(2, "optimize", new String[]{"sfc /scannow", "runas /user:Administrator dism /online /cleanup-image /restorehealth", "netsh winsock reset", "ipconfig /flushdns"}, new String[]{"sudo apt-get update", "sudo apt-get install -f", "sudo systemd-resolve --flush-caches"});
     private final Integer id;
     private final String name;
